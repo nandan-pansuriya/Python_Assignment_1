791536779ca6942cdee3480b1134bbce1028cf86
Nandan Pansuriya <pansuriyanandan01@gmail.com>
2025-06-15 19:23:47 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print(f"Addition: {a+b}")
+print(f"subtraction: {a-b}")
+print(f"Multiplication: {a*b}")
+
+if b !=0 :
+    print(f"Division: {a/b}")
+else:
+    print("cannot divide by zero.")
